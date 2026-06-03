f88de193332738e64f915d7391a074040c333955
John Rey Baylen <john.rey.baylen.pro@gmail.com>
2026-06-03 12:56:07 +0800

    basic: create generics

---
+++ basics/generics.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+type Number interface {
+	uint | float64 | int
+}
+
+func sum[T Number](a T, b T) T {
+	return a + b
+}
+
+func values[K comparable, V int](data map[K]V) []V {
+	values := make([]V, 0, len(data))
+
+	for _, v := range data {
+		values = append(values, v)
+	}
+
+	return values
+}
+
+func generics_main() {
+	fmt.Println("Sum:", sum(1.1, 1.2))
+	fmt.Println("Values:", values(map[string]int{"a": 1, "b": 2, "c": 3}))
+}
