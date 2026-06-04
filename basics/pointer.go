a85ca585ecc7a61e3ff4644043660c87e4eaa555
John Rey Baylen <john.rey.baylen.pro@gmail.com>
2026-06-05 03:02:20 +0800

    basic: create pointer

---
+++ basics/pointer.go
@@ -0,0 +1,36 @@
+package main
+
+type Book struct {
+	title  string
+	author string
+}
+
+func (b *Book) setTitle(title string) {
+	b.title = title
+}
+
+func (b *Book) setAuthor(author string) {
+	b.author = author
+}
+
+func pointer_main() {
+	x := 10
+	y := &x
+
+	println("Value of x:", x)
+	println("Address of x:", &x)
+	println("Value of y (address of x):", y)
+	println("Dereferenced value of y (value of x):", *y)
+
+	x = 15
+	println("\nAfter changing x to 15:")
+	println("Value of x:", x)
+	println("Dereferenced value of y (value of x):", *y)
+
+	b := &Book{}
+	b.setTitle("The Go Programming Language")
+	b.setAuthor("Alan A. A. Donovan and Brian W. Kernighan")
+
+	println("Title:", b.title)
+	println("Author:", b.author)
+}
